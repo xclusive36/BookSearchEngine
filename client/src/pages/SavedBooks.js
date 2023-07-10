3332815dbf3b1004e38b39d2fd1b23930ccd8f14
Joshua Cavell <xclusive36@gmail.com>
2023-07-10 13:59:04 -0400

    update SavedBooks page for Apollo

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,49 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import React, { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import Auth from "../utils/auth";
+import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const { loading, data } = useQuery(QUERY_ME);
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
+  const [books, setBooks] = useState([]);
+  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (data) {
+      setBooks(data.me.savedBooks);
+    }
 
-        if (!token) {
-          return false;
-        }
+    return () => saveBookIds(savedBookIds);
+  }, [data]);
 
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
+  const handleDeleteBook = async (bookId) => {
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
     };
 
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -51,58 +34,74 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      await deleteBook({ variables: { bookId }, context: headers });
+      setBooks(books.filter((book) => book.bookId !== bookId));
+      if (savedBookIds.length) {
+        setSavedBookIds(
+          savedBookIds?.filter((savedBookId) => savedBookId !== bookId)
+        );
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      if (!data) {
+        throw new Error("something went wrong!");
+      } else {
+        console.log("data", data);
+      }
     } catch (err) {
-      console.error(err);
+      console.error("try error", err);
+      console.log("mutation error", error);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
   return (
     <>
-      <div fluid className='text-light bg-dark p-5'>
+      <div className="text-light bg-dark p-5">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
       </div>
       <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
+        {loading ? (
+          <h2>LOADING...</h2>
+        ) : (
+          <>
+            <h2 className="pt-5">
+              {books.length
+                ? `Viewing ${books.length} saved ${
+                    books.length === 1 ? "book" : "books"
+                  }:`
+                : "You have no saved books!"}
+            </h2>
+            <Row>
+              {books.map((book, index) => {
+                return (
+                  <Col md="4" key={index}>
+                    <Card border="dark">
+                      {book.image ? (
+                        <Card.Img
+                          src={book.image}
+                          alt={`The cover for ${book.title}`}
+                          variant="top"
+                        />
+                      ) : null}
+                      <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                        <p className="small">Authors: {book.authors}</p>
+                        <Card.Text>{book.description}</Card.Text>
+                        <Button
+                          className="btn-block btn-danger"
+                          onClick={() => handleDeleteBook(book.bookId)}
+                        >
+                          Delete this Book!
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </>
+        )}
       </Container>
     </>
   );
